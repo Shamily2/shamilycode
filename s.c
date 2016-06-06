8d0ae58ff923e4395cd86f056f320fe913a18e6a
Shamily2 <shamily24061996@gmail.com>
2016-06-06 23:38:55 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+ int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive number");
+}
+else if(n==0)
+{
+printf("zero");
+}
+else
+{
+printf("negative number");
+}
+}
